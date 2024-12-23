1a5a38c97853734c0cb7a46e8345514adb1afddb
alaaelsoghair37 <alaa.elsoghair11@gmail.com>
2024-12-23 16:52:19 +0200

    Question02

--- Assignment02/Program.cs
+++ Assignment02/Program.cs
@@ -4,11 +4,19 @@
     {
         static void Main(string[] args)
         {
-         #region Question 1
+            #region Question 1
             //Console.WriteLine("Hello,Please Enter A Number: ");
             //int number = Convert.ToInt32(Console.ReadLine());
             //Console.WriteLine($"Your Number is { number}");
-         #endregion
+            #endregion
+
+            #region Question 2
+
+            //string word = "Alaa37";
+            //int word02 = Convert.ToInt32(word);
+            //Console.WriteLine(word02);
+            // will throw exception because we can't convert string to integer 
+            #endregion
         }
     }
 }
