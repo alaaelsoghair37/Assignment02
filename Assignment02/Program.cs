c8e2de3adb583b58112b466abc301f644a600c6d
alaaelsoghair37 <alaa.elsoghair11@gmail.com>
2024-12-23 16:51:21 +0200

    Question01

--- Assignment02/Program.cs
+++ Assignment02/Program.cs
@@ -4,7 +4,11 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+         #region Question 1
+            //Console.WriteLine("Hello,Please Enter A Number: ");
+            //int number = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine($"Your Number is { number}");
+         #endregion
         }
     }
 }
